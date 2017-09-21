ca67c7a6def85888fe856428429e297d1f561e95
tiffany <yoonyoungkim89@gmail.com>
2017-09-21 09:03:30 +0900

    store 제거

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Vuex from 'vuex';
 import axios from 'axios';
 import Buefy from 'buefy';
 import 'buefy/lib/buefy.css';
@@ -11,7 +10,6 @@ import router from './router';
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
 Vue.use(Buefy);
-Vue.use(Vuex);
 
 /* eslint-disable no-new */
 new Vue({
