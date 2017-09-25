335a7cf717593cc8c8122822c84e0914eb0b8b01
tiffany <yoonyoungkim89@gmail.com>
2017-09-25 17:41:56 +0900

    config 설정파일에 dev에 asset패스 설정 다시 추가 - 원복

--- config/index.js
+++ config/index.js
@@ -25,6 +25,7 @@ module.exports = {
     port: 3000,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
