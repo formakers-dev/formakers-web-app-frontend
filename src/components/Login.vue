3e8451e0d42f5a4c5a248297a502763489048f67
Tiffany Kim and Yena Kim <pair+yoonyoungkim89+yenarue@gmail.com>
2017-09-26 18:29:47 +0900

    Login/Logout처리 - google eslint관련 임시 주석처리

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,51 @@
 <template>
   <div class="login">
-    Login Page
+    <div id="google-signin-btn" v-show="!isLogin"></div>
+    <div id="user-info" v-if="isLogin">로그인 중인 유저 : {{userName}}</div>
+    <div id="google-signout-btn" v-show="isLogin" v-on:click="onSignOut">로그아웃</div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
 export default {
   name: 'login',
+  data() {
+    return {
+      isLogin: false,
+      userName: ''
+    }
+  },
+  methods: {
+    onSignIn: function(googleUser) {
+      const auth2 = gapi.auth2.init();
+
+      if(auth2.isSignedIn.get()) {
+        const profile = googleUser.getBasicProfile();
+        this.isLogin = true;
+        //TODO: server로 idtoken 보내기
+        //TODO: 메인화면으로 이동 - user
+        this.userName = profile.getName();
+      } else {
+        this.isLogin = false;
+      }
+    },
+    onSignOut: function() {
+      var auth2 = gapi.auth2.getAuthInstance();
+
+      auth2.signOut().then(function () {
+        console.log('User signed out.');
+      });
+      //TODO : signOut 콜백 안에 넣기
+      this.isLogin = false;
+    },
+  },
+  mounted() {
+    gapi.signin2.render('google-signin-btn', {
+      longtitle: true,
+      onsuccess: this.onSignIn
+    });
+  }
 };
 </script>
 
@@ -29,4 +68,5 @@ export default {
   a {
     color: #42b983;
   }
+
 </style>
