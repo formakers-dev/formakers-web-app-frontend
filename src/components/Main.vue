57b77d984933ca542fce618c94809883e0522ef2
Tiffany Kim <yoonyoungkim89@gmail.com>
2017-10-16 08:52:36 +0900

    button 클릭시 휴대폰으로 push하는 버튼 추가

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,6 +4,7 @@
     <button v-on:click="searchApps">Search</button>
     <p> {{message}}가 입력됨 </p>
     <p> 전체 사용자 수 {{count}}</p>
+    <button v-on:click="sendMessage">Push Push Baby~</button>
     <ul>
       <li v-for="app in apps" v-on:click="">
         {{ app.appName }}
@@ -13,37 +14,42 @@
 </template>
 
 <script>
-import Search from './Search';
-import List from './List';
-import HTTP from '../api/http-common';
+  import Search from './Search';
+  import List from './List';
+  import HTTP from '../api/http-common';
 
-export default {
-  name: 'main',
-  components: {
-    Search,
-    List,
-  },
-  data() {
-    return {
-      message: '',
-      count: 0,
-      apps: [],
-      selectedList: [],
-    };
-  },
-  created() {
-    HTTP.get('/user/count').then((result) => {
-      this.count = result.data.count;
-    });
-  },
-  methods: {
-    searchApps() {
-      HTTP.get(`/app?keyword=${this.message}`).then((result) => {
-        this.apps = result.data;
+  export default {
+    name: 'main',
+    components: {
+      Search,
+      List,
+    },
+    data() {
+      return {
+        message: '',
+        count: 0,
+        apps: [],
+        selectedList: [],
+      };
+    },
+    created() {
+      HTTP.get('/user/count').then((result) => {
+        this.count = result.data.count;
       });
     },
-  },
-};
+    methods: {
+      searchApps() {
+        HTTP.get(`/app?keyword=${this.message}`).then((result) => {
+          this.apps = result.data;
+        });
+      },
+      sendMessage() {
+        HTTP.post('/message').then((result) => {
+          console.log(result);
+        });
+      },
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -51,7 +57,6 @@ export default {
   h1, h2 {
     font-weight: normal;
   }
-
 
   a {
     color: #42b983;
