a613b445f5bc4e8a491e45e2201bf68919d1e8f8
Jason Ryu and Tiffany Kim <pair+sryu99+yoonyoungkim89@gmail.com>
2017-10-19 09:59:34 +0900

    BASE_URL환경변수를 운영체제 환경변수에 들어있는 내용으로 활용하도록 수정

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,6 +2,5 @@ const merge = require('webpack-merge');
 const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"dev"',
-  BASE_URL: '"http://localhost:8080"'
+  NODE_ENV: '"dev"'
 });
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"https://dragonserver.herokuapp.com"'
+  BASE_URL: JSON.stringify(process.env.BASE_URL || "http://localhost:8080")
 }
