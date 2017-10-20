fda28ea99b987bebd04b9c30ca1bb2c4297fe07d
Jason Ryu and Yena Kim <pair+sryu99+yenarue@gmail.com>
2017-10-20 17:31:38 +0900

    마이페이지에 로그아웃 기능 추가

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -2,18 +2,35 @@
   <div class="my-page" className="container">
     <h1>마이페이지</h1>
     <button v-on:click="onRegisterProject">프로젝트 등록</button>
+    <button v-on:click="onLogout">로그아웃</button>
+    <div>
+      {{logoutErrorMsg}}
+    </div>
   </div>
 </template>
 
 <script>
+import HTTP from '../api/http-common';
+
 export default {
   name: 'my-page',
   data() {
-    return {};
+    return {
+      logoutErrorMsg: '',
+    };
   },
   methods: {
     onRegisterProject() {
       location.href = '/register_project';
+    },
+    onLogout() {
+      HTTP.get('/auth/logout')
+        .then(() => {
+          location.href = '/';
+        })
+        .catch((err) => {
+          this.logoutErrorMsg = `${err} 로그아웃 실패`;
+        });
     },
   },
 };
