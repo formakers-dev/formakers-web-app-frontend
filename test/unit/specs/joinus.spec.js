dfcbec1e2e160f7b7b96b2d5b8da3698552142f7
Steve Jeong and Tiffany Kim <pair+optyyie+yoonyoungkim89@gmail.com>
2017-10-27 16:15:46 +0900

    - 미사용 테스트 파일 제거 - Joinus에서 sinonTest 라이브러리 제거

--- test/unit/specs/joinus.spec.js
+++ test/unit/specs/joinus.spec.js
@@ -1,16 +1,12 @@
 import Vue from 'vue';
 import sinon from 'sinon';
-
-import sinonTestFactory from 'sinon-test';
-
-const sinonTest = sinonTestFactory(sinon);
 import JoinUs from '../../../src/components/landing/JoinusDiv';
 
 describe('JoinUs Div', () => {
-  it('이메일 주소가 입력되었을 때 앱비 뉴스레터 받기 버튼을 클릭하면 addEmail함수가 실행된다', sinonTest(function () {
+  it('이메일 주소가 입력되었을 때 앱비 뉴스레터 받기 버튼을 클릭하면 addEmail함수가 실행된다', () => {
     const Constructor = Vue.extend(JoinUs);
     const JoinUsComponent = new Constructor().$mount();
-    const spy = this.spy(JoinUsComponent, 'addEmail');
+    const spy = sinon.spy(JoinUsComponent, 'addEmail');
     const button = JoinUsComponent.$el.querySelector('button.submit');
 
     // event binding
@@ -21,7 +17,7 @@ describe('JoinUs Div', () => {
 
     expect(JoinUsComponent.addEmail).to.be.a('function');
     sinon.assert.calledOnce(spy);
-  }));
+  });
 
   it('JoinUs Div가 렌더링 되었을 때 초기 데이터 확인', () => {
     const defaultData = JoinUs.data();
