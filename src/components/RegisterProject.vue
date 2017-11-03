55a842734b4a8b43e97c8f2775e94e3856831110
Steve Jeong and Yena Kim <pair+optyyie+yenarue@gmail.com>
2017-11-03 16:21:04 +0900

    인터뷰 세부일정 동적 리스트 입력 기능 추가 장소, 인터뷰일정 협의가능 체크박스 추가 프로젝트 등록시 입력 데이터값 테스트 추가

--- src/components/RegisterProject.vue
+++ src/components/RegisterProject.vue
@@ -24,6 +24,9 @@
     <p>인터뷰 타입 선택</p>
     <input v-model="project.interview.type" placeholder=""/>
     <p>인터뷰 장소 선택</p>
+    <div class="field">
+      <b-checkbox v-model="project.interview.location_negotiable">협의가능</b-checkbox>
+    </div>
     <input v-model="project.interview.location" placeholder=""/>
     <p>인터뷰 모집기간</p>
     <b-field label="모집 시작일">
@@ -39,6 +42,9 @@
       </b-datepicker>
     </b-field>
     <p>인터뷰 진행기간</p>
+    <div class="field">
+      <b-checkbox v-model="project.interview.date_negotiable">인터뷰이가 원하는 날짜로 협의</b-checkbox>
+    </div>
     <b-field label="인터뷰 시작일">
       <b-datepicker
         icon="today"
@@ -52,6 +58,14 @@
       </b-datepicker>
     </b-field>
     <p>인터뷰 세부일정 입력</p>
+    <ul>
+      <li v-for="(item, index) in plans">
+        <input id="interview-minutes" type="number" v-model.number="plans[index].minute"/>
+        분
+        <input id="interview-plan" type="text" v-model="plans[index].plan"/>
+      </li>
+    </ul>
+    <button class="add-interview-plan-button" v-on:click="addInterviewSchedule">일정 추가</button>
     <br/>
     <button class="temporary-save-button" v-on:click="tempRegisterProject">임시저장</button>
     <button class="save-button" v-on:click="registerProject">프로젝트 등록</button>
@@ -83,7 +97,6 @@ export default {
   data() {
     return {
       project: {
-        customerId: '',
         name: '',
         introduce: '',
         images: [],
@@ -92,11 +105,16 @@ export default {
         interview: {
           type: '',
           location: '',
+          location_negotiable: false,
           open_date: '',
           close_date: '',
           start_date: '',
           end_date: '',
-          plans: [],
+          date_negotiable: false,
+          plans: [{
+            minute: 0,
+            plan: '',
+          }],
         },
         status: '',
         interviewer: {
@@ -114,6 +132,10 @@ export default {
         start_date: new Date(),
         end_date: new Date(),
       },
+      plans: [{
+        minute: 0,
+        plan: '',
+      }],
     };
   },
   created() {
@@ -144,6 +166,8 @@ export default {
       this.project.interview.start_date = this.dateFormatter(this.date_picker.start_date);
       this.project.interview.end_date = this.dateFormatter(this.date_picker.end_date);
       this.project.status = 'registered';
+      this.project.interview.plans = this.plans;
+
       HTTP.post('/project', this.project).then(() => {
         this.$router.push('my_page');
       });
@@ -156,6 +180,12 @@ export default {
     },
     dateFormatter(date = new Date()) {
       return moment(date).format('YYYY-MM-DD');
+    },
+    addInterviewSchedule() {
+      this.plans.push({
+        minute: 0,
+        plan: '',
+      });
     },
   },
 };
--- test/unit/specs/components/RegisterProject.spec.js
+++ test/unit/specs/components/RegisterProject.spec.js
@@ -7,6 +7,44 @@ import HTTP from '../../../../src/apis/http-common';
 describe('RegisterProject Component', () => {
   const sandbox = sinon.sandbox.create();
 
+  const testData = {
+    project: {
+      name: 'old-test-project',
+      introduce: '간단소개',
+      images: ['/image1', '/image2'],
+      apps: ['com.kakao.talk'],
+      description: '프로젝트 상세 설명',
+      description_images: ['/desc/image1', '/desc/image2'],
+      interview: {
+        type: 'offline',
+        location_negotiable: false,
+        location: '향군타워 5층',
+        open_date: '2017-11-03',
+        close_date: '2017-11-03',
+        date_negotiable: false,
+        start_date: '2017-11-03',
+        end_date: '2017-11-03',
+        plans: [{
+          minute: '9999',
+          plan: '초기계획',
+        }],
+      },
+      interviewer: {
+        name: '인터뷰어',
+        url: 'interviewr image',
+        introduce: '인터뷰어소개입니다',
+      },
+      status: 'temporary',
+    },
+    date_picker: {
+      open_date: new Date('2017-10-11'),
+      close_date: new Date('2017-10-16'),
+      start_date: new Date('2017-11-01'),
+      end_date: new Date('2017-11-30'),
+    },
+    plans: [{ minute: 10, plan: '제품 소개' }, { minute: 30, plan: '테스트진행' }],
+  };
+
   it('프로젝트 등록 버튼이 클릭되었을 때, registerProject 메소드가 호출된다', () => {
     const spyOnRegisterProject = sandbox.spy(RegisterProject.methods, 'registerProject');
     const vm = getVmInstance(RegisterProject);
@@ -21,11 +59,42 @@ describe('RegisterProject Component', () => {
     const stubHttpOnPost = sandbox.stub(HTTP, 'post');
     stubHttpOnPost.withArgs('/project').returns(Promise.resolve());
 
-    const vm = getVmInstance(RegisterProject);
+    const option = {
+      data: testData,
+    };
+
+    const vm = getVmInstance(RegisterProject, option);
 
     vm.registerProject();
 
     vm.$nextTick(() => {
+      expect(vm.project.name).to.be.eql('old-test-project');
+      expect(vm.project.introduce).to.be.eql('간단소개');
+      expect(vm.project.images.length).to.be.eql(2);
+      expect(vm.project.images[0]).to.be.eql('/image1');
+      expect(vm.project.images[1]).to.be.eql('/image2');
+      expect(vm.project.apps.length).to.be.eql(1);
+      expect(vm.project.apps[0]).to.be.eql('com.kakao.talk');
+      expect(vm.project.description).to.be.eql('프로젝트 상세 설명');
+      expect(vm.project.description_images.length).to.be.eql(2);
+      expect(vm.project.description_images[0]).to.be.eql('/desc/image1');
+      expect(vm.project.description_images[1]).to.be.eql('/desc/image2');
+      expect(vm.project.interview.type).to.be.eql('offline');
+      expect(vm.project.interview.location_negotiable).to.be.eql(false);
+      expect(vm.project.interview.location).to.be.eql('향군타워 5층');
+      expect(vm.project.interview.open_date).to.be.eql('2017-10-11');
+      expect(vm.project.interview.close_date).to.be.eql('2017-10-16');
+      expect(vm.project.interview.start_date).to.be.eql('2017-11-01');
+      expect(vm.project.interview.end_date).to.be.eql('2017-11-30');
+      expect(vm.project.interview.date_negotiable).to.be.eql(false);
+      expect(vm.project.interview.plans.length).to.be.eql(2);
+      expect(vm.project.interview.plans[0].minute).to.be.eql(10);
+      expect(vm.project.interview.plans[0].plan).to.be.eql('제품 소개');
+      expect(vm.project.interview.plans[1].minute).to.be.eql(30);
+      expect(vm.project.interview.plans[1].plan).to.be.eql('테스트진행');
+      expect(vm.project.interviewer.name).to.be.eql('인터뷰어');
+      expect(vm.project.interviewer.url).to.be.eql('interviewr image');
+      expect(vm.project.interviewer.introduce).to.be.eql('인터뷰어소개입니다');
       expect(vm.project.status).to.be.eql('registered');
       sinon.assert.calledWithExactly(stubHttpOnPost, '/project', vm.project);
       done();
