8b39d1956c7ad13a7f5b9d7a5b52356f2be2de94
Steve Jeong <optyyie@gmail.com>
2017-12-20 17:26:30 +0900

    내 프로젝트 - 프로젝트 목록 디자인 적용

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -2,15 +2,21 @@
     <div id="project-item">
       <div class="project-area">
         <img v-bind:src="project.image.url" />
-        <div class="project-text-button-area">
+        <div class="project-description-area">
           <div class="text-area">
             <p class="project-name">{{project.name}}</p>
             <p class="project-introduce">{{project.introduce}}</p>
 
           </div>
           <div class="button-area">
-            <div class="button update-interview-button" v-on:click="moveToUpdateProject"> 프로젝트 수정 </div>
-            <div class="button add-interview-button" v-on:click="moveToInterviewRegister"> 인터뷰 추가 </div>
+            <div class="buttons update-interview-button" v-on:click="moveToUpdateProject">
+              <img src="static/image/edit_button.png" class="edit-button"/>
+              <span>프로젝트 수정</span>
+            </div>
+            <div class="buttons add-interview-button" v-on:click="moveToInterviewRegister">
+              <img src="static/image/add_button.png" class="add-button"/>
+              <span>인터뷰 추가</span>
+            </div>
           </div>
         </div>
       </div>
@@ -65,47 +71,95 @@ export default {
 
 <style scoped>
 #project-item {
+  width: 870px;
+  height: 234px;
+  margin: 0 auto 30px auto;
   padding-bottom: 50px;
+  border-radius: 2px;
+  background-color: #ffffff;
+  border: solid 1px #d8d8d8;
 }
 
 img {
-  width: 350px;
-  height: 200px;
+  display: inline-block;
+  width: 360px;
+  height: 234px;
 }
 
 .project-area {
-  margin-bottom: 30px;
+  display: table;
+  table-layout: fixed;
 }
 
-.project-text-button-area {
-  width:450px;
-  display: inline-block;
+.project-description-area {
+  display: table-cell;
   vertical-align: top;
-  background-color: white;
+  margin: 0;
+  width: 510px;
+  height: 234px;
 }
-.project-text-button-area .text-area {
-  padding-top: 30px;
-  padding-bottom: 30px;
-  height: 150px;
+.project-description-area .text-area {
+  height: 171px;
+  padding: 22px 71px 22px 95px;
+}
 
+.project-description-area .text-area .project-name {
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 1.59;
+  text-align: left;
+  color: #4a4a4a;
 }
-.project-text-button-area .text-area p {
-  padding-left: 30px;
-  padding-right: 30px;
-}
-.project-text-button-area .text-area .project-name {
-  font-size: 25px;
-}
-.project-text-button-area .text-area .project-introduce {
+.project-description-area .text-area .project-introduce {
+  margin-top: 18px;
   font-size: 20px;
+  line-height: 1.5;
+  text-align: left;
+  color: #4a4a4a;
+  white-space: pre;
 }
-.project-text-button-area .button-area {
-  height: 50px;
+
+.project-description-area .button-area {
+  width: 100%;
+  height: 64px;
+  margin: 0;
+  display: table;
+  table-layout: auto;
 }
-.project-text-button-area .button {
-  display: inline-block;
-  width: 195px;
-  height: 50px;
-  border: 1px solid gray;
+
+.project-description-area .button-area .buttons {
+  display: table-cell;
+  width: 50%;
+  height: 64px;
+  margin: 0;
+  vertical-align:middle;
+  overflow: hidden;
+}
+
+.project-description-area .button-area span {
+  font-size: 16px;
+  text-align: center;
+  color: #4a4a4a;
+}
+
+.buttons.update-interview-button {
+  border-top: solid 0.5px #d8d8d8;
+  border-right: solid 0.5px #d8d8d8;
+}
+.buttons.add-interview-button {
+  border-top: solid 0.5px #d8d8d8;
+}
+
+.edit-button {
+  width: 26px;
+  height: 28px;
+  vertical-align: top;
+  margin-right: 15px;
+}
+.add-button {
+  width: 30px;
+  height: 30px;
+  vertical-align: top;
+  margin-right: 15px;
 }
 </style>
