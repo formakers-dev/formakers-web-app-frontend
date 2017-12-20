852bfacc4076dd47812f172209916fd5db313260
Steve Jeong <optyyie@gmail.com>
2017-12-20 16:05:43 +0900

    내 프로젝트 - 초기화면 디자인 적용

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="my-page" class="container">
+    <div class="title-bar">
+      <img class="appbee-logo" src="static/image/appbee_logo.png"/>
+      <button v-on:click="onLogout" class="logout-button">로그아웃</button>
+    </div>
 
-    <button v-on:click="onLogout" class="logout-button">로그아웃</button>
-
-    <h1 class="title">내 프로젝트</h1>
-    <div class="project-list-area">
+    <section class="title-section">
+      <h1 class="title">내 프로젝트</h1>
       <hr class="title-underline" />
+    </section>
+
+    <section class="content-section">
       <div>
         {{logoutErrorMsg}}
       </div>
@@ -17,9 +22,12 @@
         <p class="register-project-button-icon">+</p>
         <p class="register-project-button-text">프로젝트를 추가하세요</p>
       </div>
+    </section>
 
-    </div>
-  </div>
+    <footer class="footer-area">
+      <span class="footer-text">Copyright © 2017 | Privacy | All right reserved AppBee</span>
+    </footer>
+</div>
 </template>
 
 <script>
@@ -64,35 +72,75 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .logout-button {}
+  .title-bar {
+    height: 48px;
+    background-color: #ffcd00;
+  }
+
+  .appbee-logo {
+    width: 63px;
+    height: 25px;
+    float: left;
+    margin-top: 16px;
+    margin-left: 25px;
+  }
+  .logout-button {
+    float: right;
+  }
   .title {
+    height: 36px;
+    margin-top: 29px;
+    margin-bottom: 27px;
     font-family: BMJUAOTF;
-    font-size: 30px;
+    font-size: 36px;
+    color: #4a4a4a;
     font-weight: normal;
   }
-  .project-list-area {
-    background-color: lightgray;
-    padding-bottom: 20px;
+
+  .title-section {
+  }
+
+  .content-section {
+    min-height: 1000px;
+    padding-top: 60px;
+    background-color: #f5f5f5;
   }
   .title-underline {
-    height: 3px;
-    width: 200px;
-    background-color: black;
-    margin-left: auto;
-    margin-right: auto;
+    width: 205px;
+    height: 2.5px;
+    border: solid 2.5px #4a4a4a;
+    margin: 0 auto;
+
   }
   .register-project-button-area {
-    width: 800px;
-    margin: 30px auto;
-    background-color: white;
-    padding-top: 30px;
-    padding-bottom: 30px;
-    border: 3px dashed gray;
+    width: 870px;
+    height: 234px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: #ffffff;
+    border: dashed 1px #979797;
+    border-radius: 2px;
   }
   .register-project-button-icon {
-    font-size: 100px;
+    font-size: 144px;
+    line-height: 144px;
+    color: #4a4a4a;
   }
   .register-project-button-text {
+    margin-top: 38px;
     font-size: 20px;
   }
+
+  .footer-area {
+    height: 40px;
+    background-color: #808080;
+  }
+  .footer-text {
+    opacity: 0.8;
+    font-size: 14px;
+    text-align: center;
+    line-height: 40px;
+    color: #ffffff;
+  }
+
 </style>
