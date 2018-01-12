614e9843f58bb3279eae917d94f905f281d3ddd0
Jason Ryu and Tiffany Kim <pair+sryu99+yoonyoungkim89@gmail.com>
2018-01-12 18:31:22 +0900

    CompleteProjectRegistration 페이지에서 projectId 못받아오는 오류 해결

--- src/routers/index.js
+++ src/routers/index.js
@@ -57,6 +57,7 @@ export default new Router({
       name: 'CompleteProjectRegistration',
       beforeEnter: requireAuth,
       component: CompleteProjectRegistration,
+      props: true,
     },
   ],
   scrollBehavior() {
