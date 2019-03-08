519e803704715cc8784a48a7d3df9a973ad90301
Jake Kim and Yena Kim <pair+copyx00+yenarue@gmail.com>
2019-03-08 12:35:05 +0900

    chore: Vue.js를 위한 Heroku CD 관련 작업 추가

--- .circleci/setup-branch-and-commit.sh
+++ .circleci/setup-branch-and-commit.sh
@@ -15,6 +15,10 @@ echo "add deploy for $VERSION"
 git config --global user.email "appbee0908@gmail.com"
 git config --global user.name "AppBee Dragon"
 
+### [For only Vue.js] Heroku의 Express 환경 구성을 위한 Dummy 파일들
+cp .circleci/package.json dist/
+cp .circleci/server.js dist/
+
 cp .gitignore .gitignore_original
 sed -i '/dist/d' .gitignore
 git add dist && git commit -m "add deploy for $VERSION"
